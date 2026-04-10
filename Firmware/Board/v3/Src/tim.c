82e2d2730788c2e3dfd25b4b040b747ac5c5a4b5
Henrique Anisio <rickcesar12@live.com>
2026-04-10 19:29:11 -0300

    ajuste do tim

--- Firmware/Board/v3/Src/tim.c
+++ Firmware/Board/v3/Src/tim.c
@@ -432,15 +432,18 @@ void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef* tim_encoderHandle)
 
   if(tim_encoderHandle->Instance==TIM3)
   {
-  /* USER CODE BEGIN TIM3_MspInit 0 */
-
-  /* USER CODE END TIM3_MspInit 0 */
-    /* TIM3 clock enable */
     __HAL_RCC_TIM3_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /* USER CODE BEGIN TIM3_MspInit 1 */
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
 
-  /* USER CODE END TIM3_MspInit 1 */
+    GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_5;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
+
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
   }
   else if(tim_encoderHandle->Instance==TIM4)
   {
