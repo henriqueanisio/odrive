b1d9bf79f49daa06c18d9e5d0e183e1ca875e823
Henrique Anisio <rickcesar12@live.com>
2026-04-11 09:36:07 -0300

    encorder

--- Firmware/Board/v3/Src/tim.c
+++ Firmware/Board/v3/Src/tim.c
@@ -433,17 +433,6 @@ void HAL_TIM_Encoder_MspInit(TIM_HandleTypeDef* tim_encoderHandle)
   if(tim_encoderHandle->Instance==TIM3)
   {
     __HAL_RCC_TIM3_CLK_ENABLE();
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-
-    GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_5;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_PULLUP;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
-
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
   }
   else if(tim_encoderHandle->Instance==TIM4)
   {
