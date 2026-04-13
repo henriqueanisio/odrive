b39f24b0aa921409c4619b2aaee0b378f92ceba4
Henrique Anisio <rickcesar12@live.com>
2026-04-13 19:23:21 -0300

    ajuste

--- Firmware/Board/v3/Src/hid_app_integration.cpp
+++ Firmware/Board/v3/Src/hid_app_integration.cpp
@@ -39,6 +39,7 @@
 #include <MotorControl/odrive_main.h>   /* odrv, Axis */
 #include <MotorControl/controller.hpp>  /* Controller::ControlMode, InputMode  */
 #include <string.h>
+#include "usbd_hid.h"
 
 /* ── Convenience accessor ───────────────────────────────────────────────────── */
 static inline Axis& axis0() { return odrv.get_axis(0); }
