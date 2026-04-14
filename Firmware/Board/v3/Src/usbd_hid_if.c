e4b0ef89b1ffafd3e9e98d008bb4169c7a59c0e8
Henrique Anisio <rickcesar12@live.com>
2026-04-13 21:06:21 -0300

    defines

--- Firmware/Board/v3/Src/usbd_hid_if.c
+++ Firmware/Board/v3/Src/usbd_hid_if.c
@@ -3,6 +3,9 @@
 #include <string.h>
 #include "hid_queue.h"
 
+#define HID_JOY_DESC_SIZE     0x13
+#define HID_FFB_DESC_SIZE     0x6E
+#define HID_VENDOR_DESC_SIZE  0x1F
 
 /* ── Combined HID Report Descriptor ──────────────────────────────────────────
  *
