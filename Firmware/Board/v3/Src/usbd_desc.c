8518489807c739b13b367c1786b5b0745c986402
Henrique Anisio <rickcesar12@live.com>
2026-04-16 17:08:35 -0300

    pid

--- Firmware/Board/v3/Src/usbd_desc.c
+++ Firmware/Board/v3/Src/usbd_desc.c
@@ -94,7 +94,7 @@
 #define USBD_VID     0x1209
 #define USBD_LANGID_STRING     1033
 #define USBD_MANUFACTURER_STRING     "ODrive Robotics"
-#define USBD_PID_FS     0x0D32          /* 0x0D32 = FFB Wheel (distinto do 0x0D33 CDC) */
+#define USBD_PID_FS     0x0D52        
 #define USBD_PRODUCT_XSTR(s) USBD_PRODUCT_STR(s)
 #define USBD_PRODUCT_STR(s) #s
 #define USBD_PRODUCT_STRING_FS Direct Drive FFB Wheel
