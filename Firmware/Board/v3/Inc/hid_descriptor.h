9c7d11ea9bd0a758536f96643d30774816faec5e
Henrique Anisio <rickcesar12@live.com>
2026-04-19 14:31:04 -0300

    teste

--- Firmware/Board/v3/Inc/hid_descriptor.h
+++ Firmware/Board/v3/Inc/hid_descriptor.h
@@ -58,7 +58,8 @@ __ALIGN_BEGIN static uint8_t HID_ReportDesc_FS[] __ALIGN_END __attribute__((unus
         HID_REPORT_COUNT (5),
         HID_INPUT (DATA, VARIABLE, ABSOLUTE),
         
-        HID_REPORT_COUNT (3),
+        HID_REPORT_SIZE (3),
+        HID_REPORT_COUNT (1),
         HID_INPUT (CONSTANT, VARIABLE, ABSOLUTE),
 
         HID_USAGE (EFFECT_PLAYING),
@@ -66,7 +67,7 @@ __ALIGN_BEGIN static uint8_t HID_ReportDesc_FS[] __ALIGN_END __attribute__((unus
         HID_LOGICAL_MAXIMUM1 (1),
         HID_PHYSICAL_MINIMUM1 (0),
         HID_PHYSICAL_MAXIMUM1 (1),
-        HID_REPORT_SIZE (1),
+        HID_REPORT_SIZE (8),
         HID_REPORT_COUNT (1),
         HID_INPUT (DATA, VARIABLE, ABSOLUTE),
         
@@ -75,7 +76,7 @@ __ALIGN_BEGIN static uint8_t HID_ReportDesc_FS[] __ALIGN_END __attribute__((unus
         HID_LOGICAL_MAXIMUM1 (40),
         HID_PHYSICAL_MINIMUM1 (1),
         HID_PHYSICAL_MAXIMUM1 (40),
-        HID_REPORT_SIZE (7),
+        HID_REPORT_SIZE (8),
         HID_REPORT_COUNT (1),
         HID_INPUT (DATA, VARIABLE, ABSOLUTE),
       HID_END_COLLECTION (LOGICAL),
@@ -148,7 +149,7 @@ __ALIGN_BEGIN static uint8_t HID_ReportDesc_FS[] __ALIGN_END __attribute__((unus
           HID_USAGE(STEERING),
           HID_LOGICAL_MINIMUM1 (0),
           HID_LOGICAL_MAXIMUM1 (1),
-          HID_REPORT_SIZE (1),
+          HID_REPORT_SIZE (8),
           HID_REPORT_COUNT (1),
           HID_OUTPUT(DATA, VARIABLE, ABSOLUTE),
         HID_END_COLLECTION (LOGICAL),
--- Firmware/Board/v3/Src/usb_report_handler.c
+++ Firmware/Board/v3/Src/usb_report_handler.c
@@ -27,8 +27,15 @@ uint8_t HID_GetReport(USBD_HandleTypeDef *pdev, uint16_t wValue) {
       return TRUE;
     }
     case PID_BLOCK_LOAD_REPORT_ID: {
-      PID_BlockLoadReport data = *FFB_GetPidBlockLoad();
-      USBD_CtlSendData(pdev, (uint8_t *)&data, sizeof(PID_BlockLoadReport));
+      uint8_t buf[5];
+
+      buf[0] = PID_BLOCK_LOAD_REPORT_ID; // ID = 8
+      buf[1] = 1; // effect index (use fixo pra teste)
+      buf[2] = 1; // status = SUCCESS
+      buf[3] = 0xFF; // RAM low
+      buf[4] = 0xFF; // RAM high
+
+      USBD_CtlSendData(pdev, buf, 5);
       return TRUE;
     }
     default:
